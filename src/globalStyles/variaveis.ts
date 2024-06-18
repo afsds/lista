2ba32ab2d0bd05d52fb0e3b50003ad01a2ffe4e4
Andre <andrefilipisilveiradesousa@gmail.com>
2024-06-18 15:18:59 -0300

    projeto

---
+++ src/globalStyles/variaveis.ts
@@ -0,0 +1,11 @@
+export default {
+  secaoPrincipalCor: '#696969',
+  barraLateralCor: '#008000',
+  botaoSalvarCor: '#7CFC00',
+  botaoSairDeletar: '#FF0000',
+  botaoEditarCor: '#FFD700',
+  botaoLetraCor: '#808080',
+  dadosLetraCor: '#000',
+  fundoCardCor: '#7FFFD4',
+  brandingColor: '#ADFF2F'
+}
